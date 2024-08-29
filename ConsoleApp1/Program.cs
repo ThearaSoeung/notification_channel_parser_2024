305a0fd65e6456e0858e9257f0c69dc2fb051e8b
Theara <thearasoeung123@gmail.com>
2024-08-29 22:44:26 +0700

    Initial commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
-int a = 3;
-int b = 4;
+class Program
+{
+    private static readonly string[] ValidChannels = { "BE", "FE", "QA", "Urgent" };
+    private static readonly Regex TagRegex = new Regex(@"\[(.*?)\]", RegexOptions.Compiled);
 
-Console.WriteLine(a+b);
+    private static void Main(string[] args)
+    {
+        const string message = "[BE][QA][HAHA][Urgent] there is error";
+        HashSet<string> parsedChannels = ParseNotificationChannels(message);
+        Console.WriteLine(string.Join(", ", parsedChannels));
+    }
+
+    private static HashSet<string> ParseNotificationChannels(string message)
+    {
+        var channels = new HashSet<string>();
+
+        foreach (Match match in TagRegex.Matches(message))
+        {
+            string tag = match.Groups[1].Value;
+            if (Array.Exists(ValidChannels, channel => channel == tag))
+            {
+                channels.Add(tag);
+            }
+        }
+
+        return channels;
+    }
+}
